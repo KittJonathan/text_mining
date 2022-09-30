7b29daacd5d1dd391df9de60cc3a9beb91768bbc
Compte stagiaire2 <eleve2@inrae.fr>
2022-09-30 15:10:42 +0200

    update

---
+++ R/plots.R
@@ -0,0 +1,31 @@
+tidy_books <- austen_books() %>% 
+  group_by(book) %>% 
+  mutate( 
+    linenumber = row_number(), 
+    chapter = cumsum(str_detect(text,  
+                                regex("^chapter [\\divxlc]",  
+                                      ignore_case = TRUE)))) %>% 
+  ungroup() %>% 
+  unnest_tokens(word, text) 
+
+tidy_books
+
+bing_word_counts <- tidy_books %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  count(book, chapter, word, value) %>% 
+  ungroup() |> 
+  filter(chapter != 0)
+
+chapter_scores <- bing_word_counts |> 
+  mutate(score = n * value) |> 
+  group_by(book, chapter) |> 
+  summarise(ch_score = sum(score))
+
+chapter_scores
+
+ggplot(chapter_scores) +
+  geom_tile(aes(x = chapter, y = book, fill = ch_score),
+            colour = "white", height = 0.75) +
+  scale_fill_gradient(low = "black", high = "cyan2") +
+  labs(y = "") +
+  theme_minimal()
